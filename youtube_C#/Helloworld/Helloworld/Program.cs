bf078d46daaf3fa11e39e6f450c9862d157c4c6a
jihyelisa <jihyelisak@gmail.com>
2023-11-12 07:37:08 +0900

    add C# study folder

---
+++ youtube_C#/Helloworld/Helloworld/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Textbook
+{
+    public class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ youtube_C#/practice/practice/Program.cs
@@ -0,0 +1,17 @@
+﻿// for문을 사용하여 1부터 4까지의 팩토리얼 값 출력
+
+using System;
+
+class forFactorial
+{
+    static void Main()
+    {
+        int fact = 1;
+
+        for (int i = 1; i <= 4; i++)
+        {
+            fact *= i;
+            Console.WriteLine($"{i}! = {fact}");
+        }
+    }
+}
